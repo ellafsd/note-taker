cd5de0838e9a31c77f810e04aa3f9762f0a23520
ellafsd <ellagirin82@gmail.com>
2024-05-20 18:44:20 -0400

    changes added

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const db = require('../db/db.json');
+
+
+router.get('/notes', (req, res) =>{
+    res.json(db);
+       
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
+
+
